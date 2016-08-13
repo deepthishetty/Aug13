3cdb6e8f47542b5fd68e5dce2dfe426182f90924
Deepthi <deepthish24@gmail.com>
2016-08-13 17:53:07 +0530

    dfgd

--- Autom/src/qsp/Jen.java
+++ Autom/src/qsp/Jen.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+
 public class Jen {
 	@Test
 	public void login()
@@ -12,6 +14,8 @@ public class Jen {
 	System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 	WebDriver driver=new FirefoxDriver();
 	driver.get("http://www.gmail.com");
+	System.out.println("hi");
+	//ExtentReports e=
 	
 	}
 }
