f32c55ffdefe871dd4225e83823c58aaa751bd02
Deepthi <deepthish24@gmail.com>
2016-08-13 18:34:28 +0530

    ty

--- Autom/src/qsp/Jen.java
+++ Autom/src/qsp/Jen.java
@@ -2,11 +2,16 @@ package qsp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class Jen {
+	ExtentReports e=new ExtentReports("./ExtentReport/ExtentReport.html");
+	
 	@Test
 	public void login()
 	{
@@ -14,9 +19,24 @@ public class Jen {
 	System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 	WebDriver driver=new FirefoxDriver();
 	driver.get("http://www.gmail.com");
-	System.out.println("hi");
-	ExtentReports e=new ExtentReports("./ExtentReport.html");
-e.startTest("test1");
+	//stem.out.println("hi");
+	ExtentTest t = e.startTest("test1");
+	 t.log(LogStatus.PASS,"hi");
+	 e.endTest(t);
+	 //e.flush();
+
+	
+	}
+	
+	@Test
+	public void login1()
+	{
+		ExtentTest t1 = e.startTest("test2");
+		t1.log(LogStatus.FAIL,"bye");
+		 e.endTest(t1);                       
+		 e.flush();
+	//Assert.fail();
+
 	
 	}
 }
