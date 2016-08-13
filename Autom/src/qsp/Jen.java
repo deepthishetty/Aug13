f4ee2c0fa1acaae0f453ecad847568a8761136aa
Deepthi <deepthish24@gmail.com>
2016-08-13 15:32:56 +0530

    hi

---
+++ Autom/src/qsp/Jen.java
@@ -0,0 +1,17 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Jen {
+	@Test
+	public void login()
+	{
+	
+	System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	driver.get("http://www.gmail.com");
+	
+	}
+}
